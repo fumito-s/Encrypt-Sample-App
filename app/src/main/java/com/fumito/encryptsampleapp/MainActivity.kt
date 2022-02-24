4cd2150cfa545da0ee887ae5aa30997adb64a448
fumito-s <fmt.fumito@gmail.com>
2022-02-24 22:44:08 +0900

    Add lists show encryption mode

--- app/src/main/java/com/fumito/encryptsampleapp/MainActivity.kt
+++ app/src/main/java/com/fumito/encryptsampleapp/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,11 +31,44 @@ class MainActivity : ComponentActivity() {
             EncryptSampleAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    DisplayColumn(text = PLAIN_TEXT)
+                    ModeList(
+                        items = listOf(
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                            "AES/CBC/PKCS7Padding",
+                        )
+                    )
                 }
             }
         }
     }
+}
+
+@Composable
+fun ModeList(items: List<String>) {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        items.forEach {
+            ModeItem(it)
+            Divider(color = Color.Gray)
+        }
+    }
+}
+
+@Composable
+fun ModeItem(name: String) {
+    Text(text = name, modifier = Modifier.padding(24.dp))
 }
 
 @Composable
